78ade3b1c2bb654a43b38edb074fa234b2ea9617
rodrigoRolim <rodrigorolimveras@92gmail.com>
2024-08-26 13:21:49 -0300

    create validation custom hook

---
+++ src/validation/useValidator.jsx
@@ -0,0 +1,17 @@
+export class UseValidator {
+  constructor(value) {
+    this.error = ''
+  }
+
+  isRequired(value, message) {
+    this.error = !value ? message : ''
+
+    return this
+  }
+  isEmail(value, message) {
+    const emailRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
+    this.error = !value && !emailRegex.test(this.value) ? message : ''
+
+    return this
+  }
+}
