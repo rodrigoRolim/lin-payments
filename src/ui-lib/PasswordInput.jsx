0f91502dfcbd52946bcded1cb46c52523051423c
rodrigoRolim <rodrigorolimveras@92gmail.com>
2024-08-26 13:21:18 -0300

    create password input

---
+++ src/ui-lib/PasswordInput.jsx
@@ -0,0 +1,26 @@
+import { useCallback } from "react"
+import LHint from "./LHint"
+import './PasswordInput.scss'
+
+export default function TextInput({ autoComplete, describedBy, label, hint, placeholder, value, setValue, inputMode }) {
+  const handlerTextInput = useCallback((event) => {
+    setValue(event.target.value)
+  }, [value])
+
+  return (
+    <label className="password-input">
+      <span className="label">{label}</span>
+      <input
+        className="input"
+        type="password"
+        aria-describedby={describedBy}
+        placeholder={placeholder}
+        autoComplete={autoComplete}
+        inputMode={inputMode}
+        value={value}
+        onInput={handlerTextInput}
+      />
+      <LHint id={describedBy} text={hint.text} className="hints"/>
+    </label>
+  )
+}
