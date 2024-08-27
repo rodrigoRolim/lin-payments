2073c304beda0271e1e921a732eb80f4cfde48da
rodrigoRolim <rodrigorolimveras@92gmail.com>
2024-08-26 21:11:58 -0300

    create a client

--- src/pages/ClientCreating.jsx
+++ src/pages/ClientCreating.jsx
@@ -1,9 +1,11 @@
 import TextInput from '../ui-lib/TextInput'
 import EmailInput from '../ui-lib/EmailInput'
-import DateInput from '../ui-lib/DateInput'
+import DatePicker from '../ui-lib/DatePicker'
 import PasswordInput from '../ui-lib/PasswordInput'
 
 import { useState } from 'react'
+import LButton from '../ui-lib/LButton'
+import PhoneInput from '../ui-lib/PhoneInput'
 
 export default function ClientCreating() {
   // fullname, email, cpf, birthdate, address and password
@@ -12,7 +14,8 @@ export default function ClientCreating() {
     email: { text: '' },
     cpf: { text: '' },
     birthdate: { text: '' },
-    password: { text: '' }
+    password: { text: '' },
+    phone: { text: '' }
   }
   
   const [validation, setValidation] = useState(fieldValidations)
@@ -21,14 +24,21 @@ export default function ClientCreating() {
   const [cpf, setCpf] = useState('')
   const [birthdate, setBirthdate] = useState('')
   const [password, setPassword] = useState('')
+  const [phone, setPhone] = useState('')
+
+  const confirmClientCreating = (event) => {
+    event.preventDefault()
+  }
 
   return (
-    <form>
+    <form onSubmit={confirmClientCreating}>
       <TextInput label="nome completo" hint={validation.fullname} value={fullname} setValue={setFullname} />
       <EmailInput label="email" hint={validation.email} value={email} setValue={setEmail} />
       <TextInput label="cpf" hint={validation.cpf} value={cpf} setValue={setCpf} />
-      <DateInput label="data de nascimento" hint={validation.birthdate} value={birthdate} setValue={setBirthdate} />
+      <DatePicker label="data de nascimento" hint={validation.birthdate} value={birthdate} setValue={setBirthdate} />
       <PasswordInput label="senha" hint={validation.password} value={password} setValue={setPassword} />
+      <PhoneInput label="celular" hint={validation.phone} value={phone} setValue={setPhone} />
+      <LButton text="confirmar" />
     </form>
   )
 }
